3c8b15991cacd677b1d833bd47f14e486a26f0ba
EricSchles <ericschles@gmail.com>
2014-11-11 12:18:18 -0800

    first example completed

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+#an example application completely lifted from:
+#http://flask.pocoo.org/
 
 @app.route("/")
 def hello():
     return "Hello nycpython people!  You are all awesome!"
 
 if __name__=='__main__':
-    app.run(debug=True) #find errrors + reloads pages when there are changes
+    app.run(debug=True) 
+    # debug=true - find errrors + reloads pages when there are changes
 
