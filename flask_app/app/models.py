3eab0a69922011d30c9010bc60f2247e1de9fe28
EricSchles <ericschles@gmail.com>
2014-11-26 08:57:02 -0800

    updating

--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from app import app
 
-app.config['DATABASE'] = 'database.db'
 db = SQLAlchemy(app)
 
 class AccountHolder(db.Model):
